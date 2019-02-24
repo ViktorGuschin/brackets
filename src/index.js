679139cad107475e324931aa724b8f52b4077109
ViktorGuschin <groftblr@gmail.com>
2019-02-24 21:26:11 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+  const arrBrackets = bracketsConfig.map(el => el[0] + el[1]);
+  while (str.length !== 0) {
+    let hasBracket = false;
+    for (let element of arrBrackets) {
+      hasBracket = str.indexOf(element) !== -1;
+      if (hasBracket) {
+        break;
+      }
+    }
+    if (!hasBracket && str.length !== 0) {
+      return false;
+    }
+    arrBrackets.forEach(element => {
+      str = str.split(element).join('');
+    });
+  }
+  return true;
+};
